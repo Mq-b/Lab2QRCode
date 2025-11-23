09ee1622d3a35b8986dee573ef747959486b6034
Zopiclone <Zopiclone.main@outlook.com>
2025-11-24 01:52:34 +0800

    修复了显示缩放错误的问题(windows 在一些分辨率（2880 * 1800）下软件显示存在问题)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "convert.h"
 
 int main(int argc, char* argv[]) {
+    QApplication::setAttribute(Qt::AA_Use96Dpi);
     QApplication app(argc, argv);
     Logging::setupLogging();
     BarcodeWidget w;
